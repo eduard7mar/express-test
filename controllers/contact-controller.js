7c5decfc471c525f04b7bb1dab82102d2ba88ce0
eduard7mar <edwardmargaryan@gmail.com>
2023-12-21 15:45:17 +0200

    MVC Pattern added

---
+++ controllers/contact-controller.js
@@ -0,0 +1,16 @@
+const Contact = require("../models/contact");
+const createPath = require("../helpers/create-path");
+
+const getContacts = (req, res) => {
+  const title = "Contacts";
+  Contact.find()
+    .then((contacts) => res.render(createPath("contacts"), { contacts, title }))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath("error"), { title: "Error" });
+    });
+};
+
+module.exports = {
+  getContacts,
+};
