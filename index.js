aa6321dff42440448f14fd1f3f838a6281c58c40
eduard7mar <edwardmargaryan@gmail.com>
2023-12-22 18:37:52 +0200

    update

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms")
 );
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public/"));
 
 app.use(methodOverride("_method"));
 
